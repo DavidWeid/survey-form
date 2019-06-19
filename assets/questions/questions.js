3a3411e337aa7b05ba7b5e5368dd74ffe208a7bb
David Weid <david.weid.2@gmail.com>
2019-06-19 10:21:18 -0400

    Update to questions.js file

---
+++ assets/questions/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: "It is easy to become absorbed in my job.",
+    type: "radio"
+  },
+  {
+    question: "I would recommend our center as a great place to work.",
+    type: "radio"
+  },
+  {
+    question: "My job provides me with a sense of meaning and purpose.",
+    type: "radio"
+  },
+  {
+    question: "I feel challenged in a way that results in personal growth.",
+    type: "radio"
+  },
+  {
+    question: "I feel like I belong here.",
+    type: "radio"
+  },
+  {
+    question: "I feel a manageable amount of stress at work.",
+    type: "radio"
+  },
+  {
+    question: "Do you feel that you are treated fairly by management?",
+    type: "radio"
+  },
+  {
+    question: "Does your fellow staff provide you support whenever needed?",
+    type: "radio"
+  },
+  {
+    question: "What are the greatest strengths of our center?",
+    type: "text"
+  },
+  {
+    question: "What areas need the most improvement at our center?",
+    type: "text"
+  }
+];
+
+module.exports = questions;
