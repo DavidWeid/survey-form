803b253e9f2d6dae63b073edb65f0c69d225bd17
David Weid <david.weid.2@gmail.com>
2019-06-20 11:37:41 -0400

    Cleaned up User model for proper validations/properties. Server now creates a User on load. DB resets if in development. User model has 2 properties: fullname (string) and completed (boolean).

--- models/user.js
+++ models/user.js
@@ -1,6 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    username: DataTypes.STRING
+    fullname: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
   });
 
   return User;
--- server.js
+++ server.js
@@ -5,14 +5,28 @@ const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const syncOptions = { force: false };
+if (process.env.NODE_ENV === "development") {
+  syncOptions.force = true;
+}
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // app.use(routes);
 
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log("Listening on port %s", PORT);
+db.sequelize
+  .sync(syncOptions)
+  .then(() => {
+    db.User.create({
+      fullname: "David Weid"
+    });
+
+    app.listen(PORT, () => {
+      console.log("Listening on port %s", PORT);
+    });
+  })
+  .catch(err => {
+    console.log(err, "Something went wrong in the server.");
   });
-});
